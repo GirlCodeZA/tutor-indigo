5985fe923c0d1f59cb0b3240ca35a86c707d4c85
Murendeni Mukwevho <murendenim@girlcode.co.za>
2023-12-06 13:09:21 +0200

    Update welcome message and primary color

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -15,8 +15,8 @@ config = {
     # Add here your new settings
     "defaults": {
         "VERSION": __version__,
-        "WELCOME_MESSAGE": "The place for all your online learning",
-        "PRIMARY_COLOR": "#3b85ff",  # cool blue
+        "WELCOME_MESSAGE": "Start your career in Tech.",
+        "PRIMARY_COLOR": "#e94154",  # cool blue
         # Footer links are dictionaries with a "title" and "url"
         # To remove all links, run:
         # tutor config save --set INDIGO_FOOTER_NAV_LINKS=[] --set INDIGO_FOOTER_LEGAL_LINKS=[]
